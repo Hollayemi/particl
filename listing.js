24e8e6552b1211181893cf8c142bd54c1331c598
[sephanyemmitty] <[sephanyemmitty@gmail.com]>
2021-05-22 23:29:25 +0100

    pushed by stephen

---
+++ listing.js
@@ -0,0 +1,53 @@
+var marketDiv = document.querySelector('.marketDiv')
+var angleUp = document.querySelector('.angleUp')
+var showMarket = document.querySelector('.showMarket')
+
+marketDiv.addEventListener('click', function(){
+    if(angleUp.style.color == "rgb(14, 194, 140)"){
+        
+        angleUp.style.transform   = "scale(-1)"
+        angleUp.style.color       = "rgb(85, 85, 85)"
+        showMarket.style.height   = "0px"
+        
+    }else{
+        angleUp.style.transform   = "scale(1)"
+        angleUp.style.color       = "rgb(14, 194, 140)"
+        showMarket.style.height   = "150px"
+
+    }
+})
+////////////////////////////////////////// wallet section//////////////////////////////////////////////
+var walletDiv        = document.querySelector('.walletDiv')
+var walletAngleUp    = document.querySelector('.walletAngleUp')
+var ShowwalletSec    = document.querySelector('.ShowwalletSec')
+
+walletDiv.addEventListener('click', function(){
+    if(walletAngleUp.style.color == "rgb(14, 194, 140)"){
+        walletAngleUp.style.transform   = "scale(-1)"
+        walletAngleUp.style.color       = "rgb(85, 85, 85)"
+        ShowwalletSec.style.height      = "0px"
+        
+    }else{
+        walletAngleUp.style.transform   = "scale(1)"
+        walletAngleUp.style.color       = "rgb(14, 194, 140)"
+        ShowwalletSec.style.height      = "200px"
+    }
+})
+
+
+
+
+
+
+
+
+///////////////////////// switching Tabs   ////////////////////////////
+
+function switchPage(PageName) {
+    var i;
+    var x = document.getElementsByClassName("mainSide");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";  
+    }
+    document.getElementById(PageName).style.display = "block";  
+}
